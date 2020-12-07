e6513a52619a5db05465598fa8f01b00b6a129d1
Matt Taylor <taylor.matt777@gmail.com>
2020-12-06 23:43:33 -0500

    day06

---
+++ day06/day06.ts
@@ -0,0 +1,53 @@
+import fs = require('fs');
+
+interface CustomsDeclarationForm {
+  questions: {
+    // the number of people on the form that answered yes to this question
+    [key: string]: number;
+  };
+  people: number;
+}
+
+function partOne(forms: CustomsDeclarationForm[]) {
+  // For each group, count the number of questions to which anyone answered
+  // "yes". What is the sum of those counts?
+  return forms.map(line => Object.keys(line.questions).length)
+      .reduce((a, b) => a + b, 0);
+}
+
+function partTwo(forms: CustomsDeclarationForm[]) {
+  // For each group, count the number of questions to which everyone answered
+  // "yes". What is the sum of those counts?
+  return forms
+      .map(form => {
+        return Object.keys(form.questions)
+            .filter(key => form.questions[key] === form.people)
+            .length;
+      })
+      .reduce((a, b) => a + b, 0);
+}
+
+function parseInput(): CustomsDeclarationForm[] {
+  let forms: CustomsDeclarationForm[] = [];
+  let questions = {};
+  let people = 0;
+
+  fs.readFileSync('day06/day06.txt', 'utf8').split('\n').forEach(line => {
+    if (line === '') {
+      forms.push({questions, people});
+      questions = {};
+      people = 0;
+    } else {
+      people++;  // each line is one person
+      for (const char of line.split('')) {
+        questions[char] = (questions[char] || 0) + 1;
+      }
+    }
+  });
+
+  return forms;
+}
+
+const forms = parseInput();
+console.log(partOne(forms));
+console.log(partTwo(forms));
