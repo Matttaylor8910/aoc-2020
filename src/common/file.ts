17c161c14264ee2c10b845dc2a3a7d0eaafa88ec
Matt Taylor <taylor.matt777@gmail.com>
2020-12-13 21:59:36 -0700

    Add support for passing "test1" to readFile, for example

--- src/common/file.ts
+++ src/common/file.ts
@@ -2,10 +2,17 @@ import * as fs from 'fs';
 
 /**
  * Assuming the second argument when calling this from the commandline is the
- * filename like: /path/dayXX.js, return the lines of the file as a sting[]
+ * filename like: /path/dayXX.js, return the lines of the file as a string[]
+ *
+ * @param filename an optional filename to load from this path instead
  */
 export function readFile(filename?: string): string[] {
-  if (!filename) {
+  if (filename) {
+    const split = process.argv[1].split('/');
+    split.pop();  // remove the filename
+    const extension = filename.split('.')[1] || 'txt';
+    filename = `${split.join('/')}/${filename}.${extension}`;
+  } else {
     filename = process.argv[1].replace('.js', '.txt');
   }
   return fs.readFileSync(filename, 'utf8').split('\n');
